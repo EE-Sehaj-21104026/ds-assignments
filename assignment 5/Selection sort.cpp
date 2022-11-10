a59b0e3c2dc23244a7f0f0d7ea3f8b8a263b03f8
EE-Sehaj-21104026 <96677133+EE-Sehaj-21104026@users.noreply.github.com>
2022-11-10 21:46:14 +0530

    Update Selection sort.cpp

--- assignment 5/Selection sort.cpp
+++ assignment 5/Selection sort.cpp
@@ -1,1 +1,45 @@
 
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void selectionsort(int arr[], int n)
+{
+    int count = 0;
+    int count1 = 0;
+    for (int i = 0; i < n; i++) // passes
+    {
+        for (int j = i + 1; j < n; j++) // comparisons
+        {
+            count++;
+            if (arr[i] > arr[j]) // swaps
+            {
+                count1++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    cout << "Number of comparisons " << count << endl; // number of comparisons
+    cout << "Number of Swaps " << count1 << endl;      // number of swaps
+}
+int main()
+{
+    int n; // n is the size of the array
+    printf("Enter the size of the array : ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the %d element of the array : ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    selectionsort(arr, n);
+    printf("Sorted array is : ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", arr[i]);
+        cout << " ";
+    }
+    return 0;
+}
